930dd306121fb2c4a2ff989b52f1f77939249ed3
Aswinthmani V <122224704+Aswinthmani2003@users.noreply.github.com>
2026-03-30 23:11:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cat > assets/js/env.js << EOF
 window.__ENV__ = {
-  SUPABASE_URL: "$SUPABASE_URL",
+  SUPABASE_URL:  "$SUPABASE_URL",
   SUPABASE_ANON: "$SUPABASE_ANON"
 };
 EOF
+echo "✅ env.js generated"
+pip install -r requirements.txt
