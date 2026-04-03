73c7ba7e281f0252f875cc4093d1804141d48f34
Aswinthmani V <122224704+Aswinthmani2003@users.noreply.github.com>
2026-04-03 13:10:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Install setuptools first to fix pkg_resources issue
+pip install --upgrade pip setuptools wheel
+
 cat > assets/js/env.js << EOF
 window.__ENV__ = {
   SUPABASE_URL:  "$SUPABASE_URL",
